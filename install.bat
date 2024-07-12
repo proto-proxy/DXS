c2b80e638c31a9cbc48ac024f0a7ed35817b948d
proto-proxy <168230508+proto-proxy@users.noreply.github.com>
2024-07-12 17:55:10 -0400

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,5 @@
+@echo off
+REM Install required Python packages
+pip install os json mmap tkinter requests Pillow customtkinter pywinstyles
+pip install Pillow --upgrade
+pip install imageenhance threading
