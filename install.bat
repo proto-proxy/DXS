962c8dafa1899b2e46cebce1f8547b2492a38c74
proto-proxy <168230508+proto-proxy@users.noreply.github.com>
2024-07-12 19:58:26 -0400

    Delete install.bat

--- install.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Install required Python packages
-pip install os json mmap tkinter requests Pillow customtkinter pywinstyles
-pip install Pillow --upgrade
-pip install imageenhance threading
