351567876a75df4bd61c31989959f63de53602d6
proto-proxy <168230508+proto-proxy@users.noreply.github.com>
2024-07-12 19:58:48 -0400

    Add files via upload

---
+++ READ.md
@@ -0,0 +1,7 @@
+Hello!
+
+this is a hex swapping tool made by prototbh and applemoesGG it is made to make hex swapping easy and quick
+
+Compile:
+
+if your shit isnt working then you might want to compile it into a exe so run the command pip install pyinstaller then pyinstaller --onefile DXS.py also rename the dxs.pyw to a .py and then it will make a exe in the dist folder just run it MAKE SURE YOU INSTALL PYTHON
---
+++ install.bat
@@ -0,0 +1,27 @@
+@echo off
+
+rem Check if Python is installed
+python --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python is not installed. Installing Python...
+    REM Download Python installer
+    bitsadmin /transfer "PythonInstaller" https://www.python.org/ftp/python/3.12.4/python-3.12.4-amd64.exe %TEMP%\python-3.12.4-amd64.exe
+
+    REM Install Python
+    start /wait %TEMP%\python-3.12.4-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
+
+    REM Check if installation was successful
+    python --version >nul 2>&1
+    if %errorlevel% neq 0 (
+        echo Failed to install Python. Exiting.
+        exit /b 1
+    )
+    echo Python installation successful.
+)
+
+rem Install required Python packages
+echo Installing required Python packages...
+pip install customtkinter
+pip install pywinstyles
+
+echo All required packages installed successfully.
