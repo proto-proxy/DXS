7a08c27ff0329a9fb84252673f677b06c8e2f730
appelmoesGG <101522959+appelmoesgg@users.noreply.github.com>
2024-07-15 18:11:04 +0200

    Update compile-to-exe.bat

--- compile-to-exe.bat
+++ compile-to-exe.bat
@@ -2,8 +2,8 @@
 pip install pyinstaller
 echo Installed pyinstaller!
 copy dxs.pyw dxs.py
-echo Created a copy of dxs.pyw -> renamed to dxs.py
+echo Created a copy of dxs.pyw -> renamed to dxs.py!
 python -m PyInstaller dxs.py --onefile
 echo Compiled dxs.py to a .exe!
-echo Check your dist folder for the .exe
+echo Check your dist folder for the .exe!
 pause
